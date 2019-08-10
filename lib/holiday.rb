6c363de71ddc92aa075743fc88406fc2f583a213
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-10 20:06:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-    puts "#{holidays}"
+      puts "#{holiday.to_s.capitalize}: #{supplies.capitalize.join}"
+    end
+    
   end
       
     
