1270f62bcbe28d419bd3526f43c7f7add02c1d5d
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-10 20:22:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  #{holiday.to_s.capitalize}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split("_").map(&:capitalize).join(" "): #{supplies.join(", ")}"
     end
   end
       #{holiday.to_s.split("_").map(&:capitalize).join(" ") "New_years"
